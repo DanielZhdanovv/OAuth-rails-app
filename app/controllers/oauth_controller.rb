e66c00171813a53ec9434cd5662a8db9db389b60
Daniel Zhdanov <danielzhdanov31@gmail.com>
2026-01-28 13:50:31 -0500

    removed presence from validate params

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -6,13 +6,13 @@ class OauthController < ApplicationController
     def validate_params
         errors = []
 
-        errors << "response_type must be code" unless params[:response_type] == "code" || params[:response_type].presence
+        errors << "response_type must be code" unless params[:response_type] == "code"
         errors << "client_id is required" if params[:client_id].blank?
         errors << "redirect_uri is required" if params[:redirect_uri].blank?
         errors << "scope is required" if params[:scope].blank?
         errors << "state is required" if params[:state].blank?
         errors << "code_challenge is required" if params[:code_challenge].blank?
-        errors << "code_challenge_method must be S256" unless params[:code_challenge_method] == "S256" || params[:code_challenge_method].presence
+        errors << "code_challenge_method must be S256" unless params[:code_challenge_method] == "S256"
 
         if errors.empty?
             render json: { success: true, message: "Params are valid" }
