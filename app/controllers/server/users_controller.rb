5f4860afb80c9f5b269946af1715963e4c3614ec
Daniel Zhdanov <danielzhdanov31@gmail.com>
2026-03-17 13:31:43 -0400

    changed users controller method

--- app/controllers/server/users_controller.rb
+++ app/controllers/server/users_controller.rb
@@ -3,17 +3,22 @@
 module Server
   # UsersController handles user related actions for the server application
   class UsersController < ApplicationController
-    def show # rubocop:disable Metrics/AbcSize
-      token = request.headers['Authorization']&.split&.last
+    # token decoding & Server::User querying done in authenticate method
+    before_action :authenticate
 
-      decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256')
+    def show
+      render json: { first_name: @user.first_name, last_name: @user.last_name, email: @user.email }
+    rescue StandardError => e
+      render json: { error: e.message }, status: e.status
+    end
+
+    def authenticate
+      @token = request.headers['Authorization']&.split&.last
+
+      decoded_token = JWT.decode(@token, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256')
       user_id = decoded_token[0]['user_id']
-      user = Server::User.find(user_id)
-      render json: { first_name: user.first_name, last_name: user.last_name, email: user.email }
-    rescue JWT::ExpiredSignature
-      render json: { error: 'Token has expired' }, status: :unauthorized
-    rescue JWT::DecodeError
-      render json: { error: 'Invalid token' }, status: :unauthorized
+
+      @user = Server::User.find(user_id)
     end
   end
 end
